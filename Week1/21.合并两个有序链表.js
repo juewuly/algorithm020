02cfbc048bbe80977ec02805840a2c0ed3a6088e
liuyang9 <liuyang9@360.cn>
2020-11-21 21:54:43 +0800

    update

--- Week1/21.合并两个有序链表.js
+++ Week1/21.合并两个有序链表.js
@@ -12,44 +12,66 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+// /**
+//  * 解法一
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
+// var mergeTwoLists = function(l1, l2) {
+//   let result;
+//   let tail;
+
+//   if (!l1)
+//     return l2;
+//   else if(!l2)
+//     return l1;
+
+//   if (l1.val < l2.val) {
+//     result = tail = l1;
+//     l1 = l1.next;
+//   } else {
+//     result = tail = l2;
+//     l2 = l2.next;
+//   }
+
+//   while (l1 && l2) {
+//     if (l1.val < l2.val) {
+//       tail = tail.next = l1;
+//       l1 = l1.next;
+//     } else {
+//       tail = tail.next = l2;
+//       l2 = l2.next;
+//     }
+//   }
+
+//   if (l1)
+//     tail.next = l1;
+//   else if (l2)
+//     tail.next = l2;
+  
+//   return result;
+// };
+
 /**
+ * 解法二
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-  let result;
-  let tail;
-
   if (!l1)
     return l2;
-  else if(!l2)
+  else if (!l2) 
     return l1;
 
   if (l1.val < l2.val) {
-    result = tail = l1;
-    l1 = l1.next;
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
   } else {
-    result = tail = l2;
-    l2 = l2.next;
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
   }
-
-  while (l1 && l2) {
-    if (l1.val < l2.val) {
-      tail = tail.next = l1;
-      l1 = l1.next;
-    } else {
-      tail = tail.next = l2;
-      l2 = l2.next;
-    }
-  }
-  
-  if (l1)
-    tail.next = l1;
-  else if (l2)
-    tail.next = l2;
-  
-  return result;
 };
 // @lc code=end
 
