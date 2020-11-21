b9a50021c14d27d4abc00830bdab2b2c5951768d
liuyang9 <liuyang9@360.cn>
2020-11-21 20:55:53 +0800

    add: 189旋转数组

---
+++ Week1/189.旋转数组.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=javascript
+ *
+ * [189] 旋转数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  let count = k % nums.length;
+  nums.unshift(...nums.splice(-count, count));
+  return nums;
+};
+// @lc code=end
+
