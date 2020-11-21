0933a5fb5017b00340dfc30118f82ed29d24e309
liuyang9 <liuyang9@360.cn>
2020-11-21 22:14:36 +0800

    updagte

--- Week1/21.合并两个有序链表.js
+++ Week1/21.合并两个有序链表.js
@@ -53,25 +53,48 @@
 //   return result;
 // };
 
+// /**
+//  * 解法二
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
+// var mergeTwoLists = function(l1, l2) {
+//   if (!l1)
+//     return l2;
+//   else if (!l2) 
+//     return l1;
+
+//   if (l1.val < l2.val) {
+//     l1.next = mergeTwoLists(l1.next, l2);
+//     return l1;
+//   } else {
+//     l2.next = mergeTwoLists(l1, l2.next);
+//     return l2;
+//   }
+// };
+
 /**
- * 解法二
+ * 解法三
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-  if (!l1)
-    return l2;
-  else if (!l2) 
-    return l1;
+  let result = tail = new ListNode(-1);
 
-  if (l1.val < l2.val) {
-    l1.next = mergeTwoLists(l1.next, l2);
-    return l1;
-  } else {
-    l2.next = mergeTwoLists(l1, l2.next);
-    return l2;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      tail = tail.next = l1;
+      l1 = l1.next;
+    } else {
+      tail = tail.next = l2;
+      l2 = l2.next;
+    }
   }
+
+  tail.next = l1 ? l1 : l2;
+  return result.next;
 };
 // @lc code=end
 
