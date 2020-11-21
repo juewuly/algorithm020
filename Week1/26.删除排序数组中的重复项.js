9ca6cbf6ef382bb4728d2007ae627c60abc6a889
liuyang9 <liuyang9@360.cn>
2020-11-21 19:18:09 +0800

    优化

--- Week1/26.删除排序数组中的重复项.js
+++ Week1/26.删除排序数组中的重复项.js
@@ -4,18 +4,43 @@
  * [26] 删除排序数组中的重复项
  */
 
+// // @lc code=start
+// /**
+//  * 解法一
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var removeDuplicates = function(nums) {
+//   let len = nums.length;
+//   for (let i = len - 1; i > 0; i--) {
+//     if (nums[i] === nums[i-1])
+//       nums.splice(i, 1);
+//   }
+//   return nums.length
+// };
+
+
 // @lc code=start
 /**
+ * 解法二
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+  if (!nums || nums.length === 0) 
+    return;
+
+  let i = 0;
+  let j = 1;
   let len = nums.length;
-  for (let i = len - 1; i > 0; i--) {
-    if (nums[i] === nums[i-1])
-      nums.splice(i, 1);
+  while (j < len) {
+    if (nums[i] !== nums[j] && j - i > 0) {
+      nums[++i] = nums[j];
+    }
+    j++;
   }
-  return nums.length
+
+  return i + 1;
 };
 // @lc code=end
 
