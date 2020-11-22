236dc6f6d9370689be0007028088808f931ad764
liuyang9 <liuyang9@360.cn>
2020-11-22 13:59:21 +0800

    update

--- Week1/66.加一.js
+++ Week1/66.加一.js
@@ -6,25 +6,43 @@
 
 // @lc code=start
 /**
+ * 解法一
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+// var plusOne = function(digits) {
+//   let carry = 1;
+//   let temp;
+//   for (let i = digits.length -1; i >= 0; i--) {
+//     temp = digits[i] + carry;
+//     if (temp === 10) {
+//       carry = 1;
+//       digits[i] = 0;
+//     } else {
+//       carry = 0;
+//       digits[i] = temp;
+//     }
+//   }
+
+//   if (carry === 1)
+//     digits.unshift(1);
+
+//   return digits;
+// };
+
+/**
+ * 解法二
  * @param {number[]} digits
  * @return {number[]}
  */
 var plusOne = function(digits) {
-  let carry = 1;
-  let temp;
   for (let i = digits.length -1; i >= 0; i--) {
-    temp = digits[i] + carry;
-    if (temp === 10) {
-      carry = 1;
-      digits[i] = 0;
-    } else {
-      carry = 0;
-      digits[i] = temp;
-    }
+    digits[i] = (digits[i] + 1) % 10;
+    if (digits[i] !== 0)
+      return digits;
   }
 
-  if (carry === 1)
-    digits.unshift(1);
+  digits.unshift(1);
 
   return digits;
 };
