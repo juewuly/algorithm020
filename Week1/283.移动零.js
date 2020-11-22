e53c52fb05b5a70cc1f081b6ddb11a3a5ec43ee4
liuyang9 <liuyang9@360.cn>
2020-11-22 13:32:19 +0800

    update

--- Week1/283.移动零.js
+++ Week1/283.移动零.js
@@ -33,17 +33,38 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var moveZeroes = function(nums) {
+//   let j = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== 0)
+//       nums[j++] = nums[i];
+//   }
+
+//   for (let i = j; i < nums.length; i++)
+//     nums[i] = 0;
+  
+//   return nums;
+// };
+
+/**
+ * 解法三
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
 var moveZeroes = function(nums) {
   let j = 0;
  
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0)
-      nums[j++] = nums[i];
+    if (nums[i] !== 0) {
+      if (i > j) {
+        nums[j] = nums[i];
+        nums[i] = 0;
+      }
+      j++;
+    }
   }
 
-  for (let i = j; i < nums.length; i++)
-    nums[i] = 0;
-  
   return nums;
 };
 // @lc code=end
